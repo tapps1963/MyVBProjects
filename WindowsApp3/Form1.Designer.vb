dcd5750018dda38483dea67c6ee54ce9e9a4b66b
tapps1963 <30828878+tapps1963@users.noreply.github.com>
2018-11-17 11:32:55 +0200

    Update Form1.Designer.vb
    aaa

--- WindowsApp3/Form1.Designer.vb
+++ WindowsApp3/Form1.Designer.vb
@@ -40,7 +40,7 @@ Partial Class Form1
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Abc", "Def", "Ghi"})
+        Me.ComboBox1.Items.AddRange(New Object() {"Abc", "Def", "Ghihhhh"})
         Me.ComboBox1.Location = New System.Drawing.Point(12, 35)
         Me.ComboBox1.Name = "ComboBox1"
         Me.ComboBox1.Size = New System.Drawing.Size(224, 21)
