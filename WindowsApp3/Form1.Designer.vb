fe20dc174345ad1662d949b7732069c9401fd25e
Shayne Chadwick Tappan <Shayne.Tappan@eoh.co.za>
2018-11-17 11:30:17 +0200

    Again

--- WindowsApp3/Form1.Designer.vb
+++ WindowsApp3/Form1.Designer.vb
@@ -24,6 +24,7 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.CheckBox1 = New System.Windows.Forms.CheckBox()
         Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.SuspendLayout()
         '
         'CheckBox1
@@ -36,11 +37,21 @@ Partial Class Form1
         Me.CheckBox1.Text = "CheckBox1"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Abc", "Def", "Ghi"})
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 35)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(224, 21)
+        Me.ComboBox1.TabIndex = 1
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.CheckBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -51,4 +62,5 @@ Partial Class Form1
 
     Friend WithEvents CheckBox1 As CheckBox
     Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
